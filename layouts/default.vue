66f82aa21595d8e20968c1affcf917c87e626b72
Kevin Orge <kevinorge@gmail.com>
2023-03-05 19:47:41 +0800

    default layout new styling: full width/height without padding, added navbar component inside a client only tag,

---
+++ layouts/default.vue
@@ -0,0 +1,20 @@
+<script setup>
+useHead({
+    bodyAttrs: {
+        style: 'height: 100vh',
+    },
+})
+</script>
+<template>
+    <div class="container-fluid h-100 p-0 bg-dominant">
+        <ClientOnly>
+            <NavBar></NavBar>
+        </ClientOnly>
+        <slot />
+    </div>
+</template>
+<style>
+#__nuxt{
+    height: 100%;
+}
+</style>
