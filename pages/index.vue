aa25616c0c05f7b6543d79e4954c3cb78d47e078
Kevin Orge <kevinorge@gmail.com>
2023-03-05 19:48:02 +0800

    sample home page with my name, linked in and github links

---
+++ pages/index.vue
@@ -0,0 +1,16 @@
+<script setup>
+useHead({
+    title: 'Kevin Orge'
+});
+</script>
+<template>
+    <div class="w-100 d-flex flex-column align-items-center" style="margin-top: 5rem;">
+        <div class="text-complement">
+            <strong class="fs-1">Kevin Orge</strong>
+            <div class="d-flex justify-content-center">
+                <a href="https://www.linkedin.com/in/kevin-orge-583a06b3" target="_blank" class="btn text-complement">Linked in</a>
+                <a href="https://github.com/nekoTheGreat" target="_blank" class="btn text-complement">Github</a>
+            </div>
+        </div>
+    </div>
+</template>
