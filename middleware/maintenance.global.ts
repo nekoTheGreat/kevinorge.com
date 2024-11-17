4dafe45c24e227fd86c92ff9e2d5b9ccccc309f7
Kevin Orge <kevinorge@gmail.com>
2024-11-17 23:55:15 +0800

    fixed infinite loop in redirect

--- middleware/maintenance.global.ts
+++ middleware/maintenance.global.ts
@@ -2,7 +2,11 @@ export default defineNuxtRouteMiddleware((to, from) => {
     if(import.meta.client) {
         const config = useRuntimeConfig();
         if(config.public.maintenance) {
-            return navigateTo("/maintenance");
+            if(to.path != '/maintenance') {
+                return navigateTo("/maintenance");
+            }
+        } else if(to.path == '/maintenance') {
+            return navigateTo('/');
         }
     }
 })
