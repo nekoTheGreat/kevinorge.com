fcba318d34eb1780b9973446d4ab1bf96f03253d
Kevin Orge <kevinorge@gmail.com>
2024-11-17 23:55:25 +0800

    simple maintenance page

---
+++ pages/maintenance.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+useHead({
+  title: 'Maintenance Mode',
+})
+</script>
+
+<template>
+  <div class="d-flex justify-content-center align-items-center" style="z-index: 10;">
+    <div style="padding: 100px 10px; height: 300px;">
+      <div class="text-center" style="position: relative; font-size: 3rem; z-index: 10;">
+        Sorry, the site is on maintenance mode.
+      </div>
+    </div>
+  </div>
+</template>
