60ff0debcd6e1df3dff74a7a8c0983d9b632c60d
Kevin Orge <kevinorge@gmail.com>
2024-11-17 21:30:15 +0800

    custom error page

---
+++ error.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import type { NuxtError } from '#app'
+
+const props = defineProps({
+  error: Object as () => NuxtError
+})
+</script>
+
+<template>
+  <NuxtLayout>
+    <div class="d-flex justify-content-center align-items-center" style="z-index: 10;">
+      <div class="text-center" style="padding: 100px 10px; height: 300px;">
+        <div style="position: relative; font-size: 3rem; z-index: 10;">
+          {{ error?.statusMessage ?? error?.statusCode ?? 'Server Error' }}
+        </div>
+        <NuxtLink to="/">Go back home</NuxtLink>
+      </div>
+    </div>
+  </NuxtLayout>
+</template>
