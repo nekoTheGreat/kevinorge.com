e28919b95f79a615bc5695b9e9621c24a5022391
Kevin Orge <kevinorge@gmail.com>
2024-11-17 23:55:02 +0800

    enable ssr

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+    ssr: true,
     runtimeConfig: {
         public: {
             maintenance: process.env.MAINTENANCE == 'true',
