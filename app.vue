0de6e41b9d611641df1312b2ba41705f5dfb79cc
Kevin Orge <kevinorge@gmail.com>
2024-11-18 09:34:22 +0800

    instead of page, use layout manager to change to maintenance mode

--- app.vue
+++ app.vue
@@ -2,6 +2,11 @@
 import 'bootstrap/scss/bootstrap.scss';
 import 'bootstrap-icons/font/bootstrap-icons.scss';
 import '~/assets/styles/app.scss';
+
+const config = useRuntimeConfig();
+if(config.public.maintenance) {
+    setPageLayout('maintenance');
+}
 </script>
 <template>
     <NuxtLayout>
---
+++ layouts/maintenance.vue
@@ -0,0 +1,20 @@
+<script setup>
+useHead({
+    bodyAttrs: {
+        style: 'height: 100vh',
+    },
+})
+</script>
+<template>
+    <div class="container-fluid h-100 p-0" style="position: relative;">
+        <div style="position: absolute; width: 280px; height: 100vh; background-color: #B6CAE7; margin-left: 100px; z-index: 1;">
+        </div>
+        <div class="d-flex justify-content-center align-items-center" style="z-index: 10;">
+            <div style="padding: 100px 10px; height: 300px;">
+            <div class="text-center" style="position: relative; font-size: 3rem; z-index: 10;">
+                Sorry, the site is on maintenance mode.
+            </div>
+            </div>
+        </div>
+    </div>
+</template>
